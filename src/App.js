c8b1ab3255788c62894516796fc88612113f999d
Ellen <eburke624@gmail.com>
2021-07-24 16:58:08 +0100

    Adding api fetch for getting data, basic data displayed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
 import Header from './components/Header';
-import Button from './components/Button';
 import SearchInput from './components/SearchInput';
 import { useState } from 'react';
 
 function App() {
-  const [showSearchButton] = useState(true);
-
-const searchPlace = (place) => {
-  console.log('saerching: ', place)
-}
+  const [showSearch] = useState(true);
 
   return (
     <div className="App">
       <Header />
-      <SearchInput />
-      {showSearchButton && <Button text='Search' />}
+      { showSearch && <SearchInput showSearch = {showSearch}/>}
+      
     </div>
   );
 }
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,8 +1,67 @@
-export const SearchInput = () => {
+import { useState } from 'react';
+
+export const SearchInput = (showSearch) => {
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY_NAME;
+    const [apiData, setApiData] = useState({});
+    const [placeToSearch, setPlaceToSearch] = useState('');
+    const [showResult, setShowResult] = useState(false);
+
+    const onSubmitInSearchInput = async (e) => {
+        e.preventDefault();
+
+        if (!placeToSearch) {
+            alert('Please provide a place name');
+            return;
+        }
+
+        const fetchData = async () => {
+            try {
+                console.log('fetch data');
+                const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${placeToSearch}`);
+                const data = await response.json()
+                setApiData(data);
+                console.log('apiData: ', apiData);
+                console.log('data: ', data);
+                setShowResult(true);
+            } catch (e) {
+                console.error(e.toString);
+            }
+        };
+        fetchData();
+
+        console.log('Searching', placeToSearch);
+
+        setPlaceToSearch('');
+        console.log('search toggle: ', showSearch);
+        showSearch = false;
+
+    }
+
     return (
-        <form className='form-control'>
-            <input type='text' placeholder='Search a place' />
-        </form>
+        <div>
+            <form className='form-control' onSubmit={onSubmitInSearchInput}>
+                <input type='text' placeholder='Enter a place name ...' onChange={(e) => setPlaceToSearch(e.target.value)} className='searchText' />
+                <input type='submit' value='Search' className='btn searchButton' />
+            </form>
+
+            {showResult && <div className='weatherResult'>
+                <h4>
+                    {apiData.name} Results
+                </h4>
+                <p>
+                    Current Temp: {apiData.['main'].['temp']}
+                </p>
+                <p>
+                    Min Temp: {apiData.['main'].['temp_min']}
+                </p>
+                <p>
+                    Max Temp: {apiData.['main'].['temp_max']}
+                </p>
+                <p>Description: {apiData.["weather"][0].description}</p>
+
+            </div>}
+        </div>
+
     )
 }
 
